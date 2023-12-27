024c2a59e7a22d235d40334aa6e6dafe2100b6a5
DenGolivets <oxytechmusic@gmail.com>
2023-12-27 09:03:55 +0200

    add swiper for videoplayer

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -10,7 +10,8 @@ const PlayerContainer = styled('div')({
   opacity: 1,
 });
 
-const VideoPlayer = ({ playlist, currentVideoIndex, handleVideoEnd }) => {
+const VideoPlayer = ({ url, handleVideoEnd }) => {
+  const [isVisible, setIsVisible] = useState(true);
 
   function getVideoIdFromUrl(url) {
     const urlParams = new URLSearchParams(new URL(url).search);
@@ -18,16 +19,16 @@ const VideoPlayer = ({ playlist, currentVideoIndex, handleVideoEnd }) => {
   }
 
   return (
-    <PlayerContainer>
-        <ReactPlayer
-        url={`https://www.youtube.com/embed/${getVideoIdFromUrl(playlist[currentVideoIndex])}`}
-        width="1000px" 
-        height="500px" 
-        playing={false} 
-        controls
-        onEnded={handleVideoEnd}
-        />
-    </PlayerContainer>
+          <PlayerContainer>
+            <ReactPlayer
+              url={`https://www.youtube.com/embed/${getVideoIdFromUrl(url)}`}
+              width="1000px"
+              height="500px"
+              playing={false}
+              controls
+              onEnded={handleVideoEnd}
+            />
+          </PlayerContainer>
   );
 };
 
